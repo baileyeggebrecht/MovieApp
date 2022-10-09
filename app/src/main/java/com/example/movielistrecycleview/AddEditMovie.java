4142262dd7586b6832026f736534a38b5b79bafd
BaileyEggebrecht <baileyteggebrecht@gmail.com>
2022-10-09 12:57:21 -0600

    Added Button Functionality for Cancel/OK

--- app/src/main/java/com/example/movielistrecycleview/AddEditMovie.java
+++ app/src/main/java/com/example/movielistrecycleview/AddEditMovie.java
@@ -2,13 +2,37 @@ package com.example.movielistrecycleview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddEditMovie extends AppCompatActivity {
+
+    Button btn_ok, btn_cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_movie);
+
+        btn_ok = findViewById(R.id.btn_ok);
+        btn_cancel = findViewById(R.id.btn_cancel);
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddEditMovie.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddEditMovie.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/movielistrecycleview/MainActivity.java
+++ app/src/main/java/com/example/movielistrecycleview/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.movielistrecycleview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_addMovie;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_addMovie = findViewById(R.id.btn_addMovie);
+
+        btn_addMovie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                Intent intent = new Intent(MainActivity.this,AddEditMovie.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
